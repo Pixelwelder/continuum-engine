0b42c915fa3e23b52b21c3b5806194e454719754
Zack Jordan <=>
2020-04-07 11:46:21 -0500

    Add test to ensure exports are correct.

--- src/js/__tests__/index.test.js
+++ src/js/__tests__/index.test.js
@@ -3,8 +3,22 @@ import {
   formatAbstractNumber, formatDictionaryNumber, formatScientificNumber
 } from '../index';
 
+/**
+ * This test does absolutely nothing of interest except ensuring that I didn't screw up the exports.
+ * It is, furthermore, hard to keep up to date.
+ */
 describe('exports', () => {
   test('it should export everything correctly', () => {
     expect(Currency).toBeFunction();
+    expect(Engine).toBeFunction();
+    expect(Entity).toBeFunction();
+    expect(EventEmitter).toBeFunction();
+    expect(Modifier).toBeFunction();
+    expect(Producer).toBeFunction();
+    expect(Reactor).toBeFunction();
+    expect(Resource).toBeFunction();
+    expect(formatAbstractNumber).toBeFunction();
+    expect(formatDictionaryNumber).toBeFunction();
+    expect(formatScientificNumber).toBeFunction();
   });
 });
