a14fbca36543b0f80f97ade37a67dfbc678bb994
Zack Jordan <=>
2020-04-15 15:48:24 -0500

    Upgrades now extend EventEmitter

--- src/js/__tests__/upgrade.test.js
+++ src/js/__tests__/upgrade.test.js
@@ -15,12 +15,15 @@ describe('Upgrade', () => {
     engine.activateUpgrade('upgrade1', funcMap);
   });
 
-  test('it should save an upgrade', () => {
-    // const engine = new Engine();
-    // const upgrade = engine.createUpgrade({
-    //   key: 'upgrade1'
-    // });
-    // engine.saveState();
-    // expect(window.localStorage.setItem).toHaveBeenCalled();
+  test('it should emit an event when activated', () => {
+    const engine = new Engine();
+    const funcMap = { upgrade1: () => console.log('upgrade!') };
+    const upgrade = engine.createUpgrade({
+      key: 'upgrade1'
+    });
+    const listener = jest.fn();
+    upgrade.on('UPGRADE_ACTIVATED', listener);
+    engine.activateUpgrade('upgrade1', funcMap);
+    expect(listener).toHaveBeenCalled();
   });
 });
--- src/js/upgrade.js
+++ src/js/upgrade.js
@@ -1,36 +1,18 @@
+import EventEmitter from './eventemitter';
+
 /**
  * One-shot item.
  */
-
-/*
-{
-  key: UPGRADE_00_00
-  meta: {
-    name: 'Advanced Hygiene',
-    description: <p>+1<PriCurrencyIcon/> per launch</p>,
-    flavor: `Turns out we can seat them even closer together!`,
-  },
-  isActive: false,
-  cost: {
-    [CURRENCIES.PRIMARY]: 10,
-    [CURRENCIES.SECONDARY]: 1
-  },
-  applyFunc: (engine) => {
-    console.log('ACTIVATED');
-  },
-  removeFunc: (engine) => {
-    console.log('DEACTIVATED');
-  }
-}
- */
-export default class Upgrade {
+export default class Upgrade extends EventEmitter {
   constructor(opts) {
+    super(opts);
+
     this.state = {
+      ...this.state,
       key: opts.key,
       meta: opts.meta || {},
       isActive: opts.isActive || false,
-      cost: opts.cost || {},
-      applyFuncName: opts.applyFuncName
+      cost: opts.cost || {}
     }
 
     this.engine = opts.engine;
@@ -39,9 +21,15 @@ export default class Upgrade {
   get key()               { return this.state.key }
   get meta()              { return this.state.meta }
   get isActive()          { return this.state.isActive }
-  set isActive(value)     { this.state.isActive = value }
   get cost()              { return this.state.cost }
-  get applyFuncName()     { return this.state.applyFuncName }
+
+  set isActive(value) {
+    this.state.isActive = value
+    this.emit('UPGRADE_ACTIVATED', {
+      obj: this,
+      key: this.state.key
+    });
+  }
 
   serialise() {
     return this.state;
